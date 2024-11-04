865fc09ccc1f8e0f25a28549bc2af02273823a59
Mark <chaseaxieez@gmail.com>
2024-11-04 18:45:12 +0800

    Install react-router and setup the routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Login from "./Login";
+import Register from "./Register";
+import Welcome from "./Welcome";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/welcome" element={<Welcome />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
