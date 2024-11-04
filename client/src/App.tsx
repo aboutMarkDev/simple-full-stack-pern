9ba4f7bcbbbd3e42e12cde0fb326d33f61dd347e
Mark <chaseaxieez@gmail.com>
2024-11-04 18:52:03 +0800

    create a root layout and moved to pages folder all of the routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,20 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./Home";
-import Login from "./Login";
-import Register from "./Register";
-import Welcome from "./Welcome";
+import Home from "./(root)/pages/Home";
+import Login from "./(root)/pages/Login";
+import Register from "./(root)/pages/Register";
+import Welcome from "./(root)/pages/Welcome";
+import RootLayout from "./(root)/RootLayout";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route index element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/welcome" element={<Welcome />} />
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/welcome" element={<Welcome />} />
+        </Route>
       </Routes>
     </>
   );
