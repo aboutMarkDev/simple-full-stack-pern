64614c7f11a707d61900052e2842780ef8fb72a5
Mark <chaseaxieez@gmail.com>
2024-11-05 11:44:10 +0800

    Install and use react-hook-form and create some validation

--- client/src/(root)/pages/Login.tsx
+++ client/src/(root)/pages/Login.tsx
@@ -1,16 +1,59 @@
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+// REMINDER:
+// NEXT TODO:
+// REACT-FORM & ZOD FOR VALIDATION MAKE THE USERNAME LIMITED TO 12 CHARACTERS
+// THEN BUILD A SERVER/BACKEND USING EXPRESS WITH JWT AND POSTGRESQL TO BECOME FAMILIAR WITH SQL.
+// THEN DEPLOY IT
+
+type LoginFormData = {
+  username: string;
+  password: string;
+};
 
 const Login = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<LoginFormData>();
+
+  const onSubmit = async (values: LoginFormData) => {
+    console.log(values);
+  };
+
   const [isPassShow, setIsPassShow] = useState(false);
   return (
     <section className="flex-grow py-5 px-3 flex items-center justify-center flex-col gap-5 text-sm">
       <h1 className="text-center font-bold text-3xl">Login</h1>
-      <form className="w-full max-w-[18rem] space-y-3">
+
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-full max-w-[18rem] space-y-3"
+      >
         <article className="space-y-1">
           <h3>Username</h3>
-          <input type="text" className="h-10 rounded-lg px-3 border w-full" />
+          <input
+            type="text"
+            className="h-10 rounded-lg px-3 border w-full"
+            {...register("username", {
+              required: "Required",
+              minLength: {
+                value: 4,
+                message: "Username must be at least 4 characters.",
+              },
+              maxLength: {
+                value: 12,
+                message: "Username must be at most 12 characters.",
+              },
+            })}
+          />
+          {errors?.username && (
+            <p className="text-sm text-red-500">{errors.username.message}</p>
+          )}
         </article>
         <article className="space-y-1">
           <div className="flex items-center justify-between">
@@ -22,7 +65,17 @@ const Login = () => {
           <input
             type={isPassShow ? "text" : "password"}
             className="h-10 rounded-lg px-3 border w-full"
+            {...register("password", {
+              required: "Required",
+              minLength: {
+                value: 4,
+                message: "Passwords must be at least 4 characters",
+              },
+            })}
           />
+          {errors?.password && (
+            <p className="text-sm text-red-500">{errors.password.message}</p>
+          )}
         </article>
         <Button type="submit" className="w-full">
           Login
--- client/src/(root)/pages/Register.tsx
+++ client/src/(root)/pages/Register.tsx
@@ -1,17 +1,58 @@
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
+type RegisterFormData = {
+  username: string;
+  password: string;
+  cpassword: string;
+};
+
 const Register = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    watch,
+  } = useForm<RegisterFormData>();
+
+  const password = watch("password");
+
   const [isPassShow, setIsPassShow] = useState(false);
   const [isCPassShow, setIsCPassShow] = useState(false);
+
+  const onSubmit = async (values: RegisterFormData) => {
+    console.log(values);
+  };
   return (
     <section className="flex-grow py-5 px-3 flex items-center justify-center flex-col gap-5 text-sm">
       <h1 className="text-center font-bold text-3xl">Registration</h1>
-      <form className="w-full max-w-[18rem] space-y-3">
+
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-full max-w-[18rem] space-y-3"
+      >
         <article className="space-y-1">
           <h3>Username</h3>
-          <input type="text" className="h-10 rounded-lg px-3 border w-full" />
+          <input
+            type="text"
+            className="h-10 rounded-lg px-3 border w-full"
+            {...register("username", {
+              required: "Required",
+              minLength: {
+                value: 4,
+                message: "Username must be at least 4 characters.",
+              },
+              maxLength: {
+                value: 12,
+                message: "Username must be at most 12 characters.",
+              },
+            })}
+          />
+          {errors?.username && (
+            <p className="text-sm text-red-500">{errors.username.message}</p>
+          )}
         </article>
         <article className="space-y-1">
           <div className="flex items-center justify-between">
@@ -23,7 +64,17 @@ const Register = () => {
           <input
             type={isPassShow ? "text" : "password"}
             className="h-10 rounded-lg px-3 border w-full"
+            {...register("password", {
+              required: "Required",
+              minLength: {
+                value: 4,
+                message: "Passwords must be at least 4 characters",
+              },
+            })}
           />
+          {errors?.password && (
+            <p className="text-sm text-red-500">{errors.password.message}</p>
+          )}
         </article>
         <article className="space-y-1">
           <div className="flex items-center justify-between">
@@ -35,7 +86,15 @@ const Register = () => {
           <input
             type={isCPassShow ? "text" : "password"}
             className="h-10 rounded-lg px-3 border w-full"
+            {...register("cpassword", {
+              required: "Required",
+              validate: (value) =>
+                value === password || "Passwords do not match",
+            })}
           />
+          {errors?.cpassword && (
+            <p className="text-sm text-red-500">{errors.cpassword.message}</p>
+          )}
         </article>
         <Button type="submit" className="w-full">
           Submit
